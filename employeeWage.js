76f4f4b38e37df8c918038dfda760a5ce377cceb
dbasumatary <basudhondip@gmail.com>
2023-03-23 14:05:51 +0530

    Added: Calculate wages for a month assuming 20 working days

--- employeeWage.js
+++ employeeWage.js
@@ -1,23 +1,21 @@
 console.log("Employee Wage Computation Problem\n")
-//UC3 - Finding daily working hours using function
+//UC4 - Calculate wages for a month assuming 20 working days
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 let employeeHours = 0;
 
 function getWorkingHours(employeeCheck){
     switch (employeeCheck) {
-
         case IS_PART_TIME:
-            console.log("The employee works Part Time");
             return PART_TIME_HOURS;
     
         case IS_FULL_TIME:
-            console.log("The employee works Full Time");
             return FULL_TIME_HOURS;
     
         default:
@@ -25,7 +23,10 @@ function getWorkingHours(employeeCheck){
     }
 }
 
-let employeeCheck = Math.floor(Math.random() * 10) % 3;
-employeeHours = getWorkingHours(employeeCheck);
+for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+    let employeeCheck = Math.floor(Math.random() * 10) % 3;
+    employeeHours += getWorkingHours(employeeCheck);
+}
+
 let employeeWage = employeeHours * WAGE_PER_HOUR;
-console.log("Employee Wage : " + employeeWage);
+console.log("Total Working Hours = "+ employeeHours + "\nTotal Employee Wage = " + employeeWage);
