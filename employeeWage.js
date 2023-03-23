76e5895afa09c0599620ceff1a14592c8392ed35
dbasumatary <basudhondip@gmail.com>
2023-03-23 14:22:59 +0530

    Added: Calculate wages for a month till 160 max hours or 20 days is reached

--- employeeWage.js
+++ employeeWage.js
@@ -1,5 +1,5 @@
 console.log("Employee Wage Computation Problem\n")
-//UC4 - Calculate wages for a month assuming 20 working days
+//UC5 - Calculate wages for a month till 160 max hours or 20 days is reached
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -7,8 +7,10 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 100;
 
-let employeeHours = 0;
+let totalEmployeeHours = 0;
+let totalWorkingDays = 0;
 
 function getWorkingHours(employeeCheck){
     switch (employeeCheck) {
@@ -23,10 +25,11 @@ function getWorkingHours(employeeCheck){
     }
 }
 
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+while(totalEmployeeHours <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
     let employeeCheck = Math.floor(Math.random() * 10) % 3;
-    employeeHours += getWorkingHours(employeeCheck);
+    totalEmployeeHours += getWorkingHours(employeeCheck);
 }
 
-let employeeWage = employeeHours * WAGE_PER_HOUR;
-console.log("Total Working Hours = "+ employeeHours + "\nTotal Employee Wage = " + employeeWage);
+let employeeWage = totalEmployeeHours * WAGE_PER_HOUR;
+console.log("Total days : " + totalWorkingDays +"\nTotal Working Hours : " + totalEmployeeHours + "\nTotal Employee Wage : " + employeeWage);
